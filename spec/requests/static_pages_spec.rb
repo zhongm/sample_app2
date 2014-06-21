2cf09beb4e39bc6f070a43c9adb2b8dabf519019
Zhongming <zhongm.guan@gmail.com>
2014-06-20 22:31:40 -0400

    generate integration test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+    
+    # A title test
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Home" )
+    end
+  end
+  
+  
+  
+end

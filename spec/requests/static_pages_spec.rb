abc1fa850e868a3694dc1c6f5c62bbecf9872819
Zhongming <zhongm.guan@gmail.com>
2014-06-20 22:40:22 -0400

    add test for title, etc.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,29 @@ describe "StaticPages" do
     end
   end
   
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    
+    #A title test
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Help")
+    end
+  end
   
-  
+  describe "About page" do
+    it "should have the contnet 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+    
+    # A title test
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "About Us")
+    end
+  end  
 end

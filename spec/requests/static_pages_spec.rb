8677df8a894593afdc36a1aadc90f8d1afc20aae
Zhongming <zhongm.guan@gmail.com>
2014-06-22 22:46:49 -0400

    add Contact page, run bundle --binstubs, etc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) {"Sample app"}
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,9 +11,14 @@ describe "StaticPages" do
     end
     
     # A title test
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Home" )
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "Home | " )
     end
   end
   
@@ -23,7 +31,7 @@ describe "StaticPages" do
     #A title test
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Help")
+      page.should have_selector('title', :text => "Help | #{base_title}")
     end
   end
   
@@ -36,7 +44,20 @@ describe "StaticPages" do
     # A title test
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "About Us")
+      page.should have_selector('title', :text => "About Us | #{base_title}")
     end
   end  
+  
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+    
+    # A title test
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Contact | #{base_title}")
+    end
+  end
 end

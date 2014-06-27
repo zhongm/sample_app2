5006b31d375f95d2b619e01b86ea0ad10b8c22df
Zhongming <zhongm.guan@gmail.com>
2014-06-26 23:45:17 -0400

    add index for users's email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@
 class User < ActiveRecord::Base
   #attr_accessible :name, :email
   
+  before_save { self.email = email.downcase }
+  
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
   
 end

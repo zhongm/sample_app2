67f88759a49b965a04e124b7a37acdb2cc5df65e
Zhongming <zhongm.guan@gmail.com>
2014-07-18 23:39:13 -0400

    finished chapter 7.2.2 - Sing up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,12 +7,7 @@ describe "UserPages" do
   after(:all) { User.delete_all }
   
   describe "profile page" do
-    
-
     let(:u) { FactoryGirl.create(:user) } 
-    
-    
-    
     before { visit user_path(u) }
     
     it { should have_content(u.name) }
@@ -24,6 +19,31 @@ describe "UserPages" do
     
     it { should have_content('Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  
+  describe "signup" do
+    
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User 16"
+        fill_in "Email", with: "user16@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
 

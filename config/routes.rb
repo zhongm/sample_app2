6e189091c687b7e7fea0f224fb3a993a9ec8afd9
Zhongming <zhongm.guan@gmail.com>
2014-07-20 23:32:25 -0400

    finished chapter 8, dropdown menu doesn't work for firefox and chrome, but it works for IE 8. I guess it works for safari

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   #get 'static_pages/home'
   root to: 'static_pages#home'
   
   #get 'users/new'
   match '/signup', to: 'users#new', via: 'get'
-
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   
   #get 'static_pages/help'
   match '/help', to: 'static_pages#help', via: 'get'

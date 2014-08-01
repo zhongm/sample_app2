8f1456868b567c14877089c94a2f630381e2ccde
Zhongming <zhongm.guan@gmail.com>
2014-08-01 01:00:48 -0400

    Order users by id ASC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   
   def index
     #@users = User.all
-    @users = User.paginate(page: params[:page])
+    @users = User.order('id ASC').paginate(page: params[:page], per_page: 10)
   end  
   
   def show
